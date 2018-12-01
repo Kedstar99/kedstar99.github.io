0438bd9d098acb9e6e5266306cc3d6caea6b466f
Krish De Souza <kd913@ic.ac.uk>
2018-12-01 02:04:39 +0000

    Fixed JS to work on individual pictures

--- card.js
+++ card.js
@@ -1,10 +1,12 @@
 $(function(){
 
-    $('#show').on('click',function(){        
-        $('.card-reveal').slideToggle('slow');
+    $('.show').on('click',function(){
+        var closest_reveal = $(this)[0].closest('.card').children[2];
+        $(closest_reveal).slideToggle('slow');
     });
     
     $('.card-reveal .close').on('click',function(){
-        $('.card-reveal').slideToggle('slow');
+        var closest_reveal = $(this)[0].parentElement;
+        $(closest_reveal).slideToggle('slow');
     });
 });
