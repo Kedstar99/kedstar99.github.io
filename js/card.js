32132ff8bbf8cf64009c302e5944a82d65da0ea9
Krish De Souza <koolkrish007+github@gmail.com>
2019-02-13 12:56:28 +0000

    Attempt to remove jquery dependency

--- js/card.js
+++ js/card.js
@@ -1,12 +1,19 @@
-$(function(){
+var open_card = function(element) {
+    var closest_reveal = element.closest('.card').children[2];
+    closest_reveal.style.height="100%"
+}
 
-    $('.show').on('click',function(){
-        var closest_reveal = $(this)[0].closest('.card').children[2];
-        closest_reveal.style.height="100%"
-    });
-    
-    $('.card-reveal .close').on('click',function(){
-        var closest_reveal = $(this)[0].parentElement.parentElement;
-        closest_reveal.style.height="0px"
-    });
-});
+var close_card = function(element) {
+    var closest_reveal = element.parentElement.parentElement;
+    closest_reveal.style.height="0px"
+}
+
+var elipsisArr = document.getElementsByClassName('show')
+for (elem in elipsisArr) {
+    elem.onclick = open_card(elem)
+}
+
+var closeArr = document.getElementsByClassName('close')
+for (elem in closeArr) {
+    elem.onclick = close_card(elem)
+}
