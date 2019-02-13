3445a10c30af694474a9563f22d1f023d6249397
Krish De Souza <koolkrish007+github@gmail.com>
2019-02-13 12:26:13 +0000

    Moved away from slidetoggle to vanillajs

--- js/card.js
+++ js/card.js
@@ -2,11 +2,11 @@ $(function(){
 
     $('.show').on('click',function(){
         var closest_reveal = $(this)[0].closest('.card').children[2];
-        $(closest_reveal).slideToggle('slow');
+        closest_reveal.style.height="100%"
     });
     
     $('.card-reveal .close').on('click',function(){
         var closest_reveal = $(this)[0].parentElement.parentElement;
-        $(closest_reveal).slideToggle('slow');
+        closest_reveal.style.height="0px"
     });
 });
