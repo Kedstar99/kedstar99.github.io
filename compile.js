76f7fe459455932bbedc03e68929091de4b5d742
Krish De Souza <krish-dev@kedstar99.xyz>
2025-06-17 12:39:47 +0100

    cleanup: remove unnecessary compile.js function

--- compile.js
+++
@@ -1,8 +0,0 @@
-const pug = require('pug');
-const fs = require('fs');
-
-const indexPug = pug.compileFile('pug/index.pug')
-const projectsPug = pug.compileFile('pug/projects.pug')
-
-fs.writeFileSync("./index.html", indexPug())
-fs.writeFileSync("./projects.html", projectsPug())
